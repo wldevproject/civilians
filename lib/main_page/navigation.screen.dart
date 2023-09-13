d8aa9ba520791c637e42be82523fba5237886ef3
wlDev <joefachrizal@gmail.com>
2023-09-13 16:56:47 +0700

    fix: nav bar optimize

--- lib/main_page/navigation.screen.dart
+++ lib/main_page/navigation.screen.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../widget/button_nav_bar.dart';
+
 class NavigationScreen extends StatefulWidget {
   const NavigationScreen({super.key});
 
@@ -9,87 +11,35 @@ class NavigationScreen extends StatefulWidget {
 
 class _NavigationScreenState extends State<NavigationScreen> {
 
-  int _selectedIndex = 0;
-
-  void _onItemTapped(int index) {
-    setState(() {
-      _selectedIndex = index;
-    });
-  }
-
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: const Center(child: Text('data')),
-      bottomNavigationBar: Container(
-        decoration: BoxDecoration(
-          borderRadius: const BorderRadius.only(
-            topLeft: Radius.circular(20.0), // Adjust the radius as needed
-            topRight: Radius.circular(20.0), // Adjust the radius as needed
-          ),
-          boxShadow: [
-            BoxShadow(
-              color: Colors.grey.withOpacity(0.5),
-              spreadRadius: 1,
-              blurRadius: 5,
-            ),
-          ],
-        ),
-        child: ClipRRect(
-          borderRadius: const BorderRadius.only(
-            topLeft: Radius.circular(20.0), // Adjust the radius as needed
-            topRight: Radius.circular(20.0), // Adjust the radius as needed
-          ),
-          child: BottomNavigationBar(
-            currentIndex: _selectedIndex,
-            onTap: _onItemTapped,
-            items: const [
-              BottomNavigationBarItem(
-                icon: Icon(Icons.home),
-                label: 'Home',
-              ),
-              BottomNavigationBarItem(
-                icon: Icon(Icons.search),
-                label: 'Search',
-              ),
-              BottomNavigationBarItem(
-                icon: Icon(Icons.favorite),
-                label: 'Favorites',
-              ),
-              BottomNavigationBarItem(
-                icon: Icon(Icons.person),
-                label: 'Profile',
-              ),
-            ],
-          ),
-        ),
-      ),
+      // bottomNavigationBar: BottomNavBarCustomV2(),
 
-      // bottomNavigationBar: BottomNavBarCustom(
-      //   backgroundColor: Colors.black87,
-      //   unselectedColor: Colors.white,
-      //   selectedColor: Colors.orange,
-      //   items: [
-      //     MyAppBarItem(
-      //       iconData: Icons.dashboard_customize_rounded,
-      //       text: 'Home',
-      //     ),
-      //     MyAppBarItem(
-      //       iconData: Icons.shopping_bag_rounded,
-      //       text: 'Cart',
-      //     ),
-      //     MyAppBarItem(
-      //       iconData: Icons.place_rounded,
-      //       text: 'Place',
-      //     ),
-      //     MyAppBarItem(
-      //       iconData: Icons.person_2_rounded,
-      //       text: 'Profile',
-      //     ),
-      //   ], onTabSelected: (index) {
-      //
-      //
-      // },),
+      bottomNavigationBar: BottomNavBarCustom(
+        backgroundColor: Colors.black87,
+        unselectedColor: Colors.white,
+        selectedColor: Colors.orange,
+        items: [
+          MyAppBarItem(
+            iconData: Icons.dashboard_customize_rounded,
+            text: 'Home',
+          ),
+          MyAppBarItem(
+            iconData: Icons.shopping_bag_rounded,
+            text: 'Cart',
+          ),
+          MyAppBarItem(
+            iconData: Icons.place_rounded,
+            text: 'Place',
+          ),
+          MyAppBarItem(
+            iconData: Icons.person_2_rounded,
+            text: 'Profile',
+          ),
+        ], onTabSelected: (index) {
+      },),
     );
   }
 }
--- lib/widget/button_nav_bar.dart
+++ lib/widget/button_nav_bar.dart
@@ -13,7 +13,6 @@ class BottomNavBarCustom extends StatefulWidget {
   });
 
   final List<MyAppBarItem> items;
-  // final List<BottomNavigationBarItem>? items2;
   final double height;
   final double iconSize;
   final Color backgroundColor;
@@ -43,21 +42,35 @@ class _BottomNavBarCustomState extends State<BottomNavBarCustom> {
       );
     });
 
-    return SizedBox(
+    return Container(
       // height: 96,
-      child: Material(
-        elevation: 10,
-        borderRadius: const BorderRadius.only(topLeft: Radius.circular(20), topRight: Radius.circular(20)),
-        child: ClipRRect(
-          borderRadius: const BorderRadius.only(topLeft: Radius.circular(20), topRight: Radius.circular(20)),
-          child: BottomAppBar(
-            shape: const CircularNotchedRectangle(),
-            color: widget.backgroundColor,
-            child: Row(
-              mainAxisSize: MainAxisSize.max,
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: items,
-            ),
+      decoration: BoxDecoration(
+        borderRadius: const BorderRadius.only(
+          topLeft: Radius.circular(20.0), // Adjust the radius as needed
+          topRight: Radius.circular(20.0), // Adjust the radius as needed
+        ),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.5),
+            spreadRadius: 0,
+            blurRadius: 2,
+            offset: const Offset(0, 0),
+          ),
+        ],
+      ),
+      child: ClipRRect(
+        borderRadius: const BorderRadius.only(
+          topLeft: Radius.circular(20.0), // Adjust the radius as needed
+          topRight: Radius.circular(20.0), // Adjust the radius as needed
+        ),
+        child: BottomAppBar(
+          // shape: const CircularNotchedRectangle(),
+          color: widget.backgroundColor,
+          clipBehavior: Clip.antiAliasWithSaveLayer,
+          // padding: EdgeInsets.zero,
+          child: Row(
+            mainAxisSize: MainAxisSize.max,
+            children: items,
           ),
         ),
       ),
@@ -74,23 +87,27 @@ class _BottomNavBarCustomState extends State<BottomNavBarCustom> {
 
     return Expanded(
       child: SizedBox(
-        height: widget.height,
-        child: InkWell(
-          onTap: () => onPressed(index),
-          focusColor : coloring,
-          hoverColor: coloring,
-          highlightColor: coloring,
-          splashColor: coloring,
-          child: Column(
-            mainAxisSize: MainAxisSize.min,
-            mainAxisAlignment: MainAxisAlignment.center,
-            children: [
-              Icon(item.iconData, color: color, size: widget.iconSize),
-              Text(
-                item.text,
-                style: TextStyle(color: color, fontSize: 11),
-              )
-            ],
+        height: double.infinity,
+        child: ClipRRect(
+          borderRadius: BorderRadius.circular(10),
+          child: Material(
+            color: Colors.transparent,
+            child: InkWell(
+              onTap: () => onPressed(index),
+              focusColor: coloring,
+              hoverColor: coloring,
+              highlightColor: coloring,
+              splashColor: coloring,
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  Icon(item.iconData, color: color, size: widget.iconSize),
+                  Text(item.text, style: TextStyle(color: color, fontSize: 11),
+                  )
+                ],
+              ),
+            ),
           ),
         ),
       ),
@@ -103,4 +120,105 @@ class MyAppBarItem {
 
   IconData iconData;
   String text;
+}
+
+class BottomNavBarCustomV2 extends StatefulWidget {
+  const BottomNavBarCustomV2({super.key});
+
+  @override
+  State<BottomNavBarCustomV2> createState() => _BottomNavBarCustomV2State();
+}
+
+class _BottomNavBarCustomV2State extends State<BottomNavBarCustomV2> {
+  int _selectedIndex = 0;
+
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      decoration: BoxDecoration(
+        borderRadius: const BorderRadius.only(
+          topLeft: Radius.circular(20.0), // Adjust the radius as needed
+          topRight: Radius.circular(20.0), // Adjust the radius as needed
+        ),
+        boxShadow: [
+          BoxShadow(
+            color: Colors.grey.withOpacity(0.5),
+            spreadRadius: 0,
+            blurRadius: 2,
+            offset: const Offset(0, 0),
+          ),
+        ],
+      ),
+      child: ClipRRect(
+        borderRadius: const BorderRadius.only(
+          topLeft: Radius.circular(20.0), // Adjust the radius as needed
+          topRight: Radius.circular(20.0), // Adjust the radius as needed
+        ),
+        child: BottomNavigationBar(
+          currentIndex: _selectedIndex,
+          onTap: _onItemTapped,
+          backgroundColor: Colors.black87,
+          selectedItemColor: Colors.deepOrange,
+          unselectedItemColor: Colors.white,
+          selectedFontSize: 10,
+          unselectedFontSize: 10,
+          type: BottomNavigationBarType.fixed,
+          enableFeedback: true,
+          items: [
+            navBarItem(
+              label: 'Home',
+              activeIcon: Icons.dashboard_customize_rounded,
+              unActiveIcon: Icons.dashboard_customize_outlined,
+            ),
+            navBarItem(
+              label: 'Cart',
+              activeIcon: Icons.shopping_bag_rounded,
+              unActiveIcon: Icons.shopping_bag_outlined,
+            ),
+            navBarItem(
+              label: 'Place',
+              activeIcon: Icons.place_rounded,
+              unActiveIcon: Icons.place_outlined,
+            ),
+            navBarItem(
+              label: 'Profile',
+              activeIcon: Icons.person_2_rounded,
+              unActiveIcon: Icons.person_2_outlined,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+
+  BottomNavigationBarItem navBarItem({
+    required String label,
+    required IconData activeIcon,
+    required IconData unActiveIcon,
+  }) {
+    return BottomNavigationBarItem(
+      label: label,
+      icon: Center(
+        child: Icon(unActiveIcon),
+      ),
+      activeIcon: Container(
+          decoration: BoxDecoration(
+            borderRadius: BorderRadius.circular(10),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.orange.withOpacity(0.3),
+                offset: const Offset(0, 4),
+                blurRadius: 20,
+              ),
+            ],
+          ),
+          child: Icon(activeIcon)),
+    );
+  }
 }
